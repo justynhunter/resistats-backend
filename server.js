dc08a0809921ccecc51cc345f5eb2bce34ac4f33
Justyn Hunter <cjhunter@me.com>
2018-11-04 15:42:08 -0600

    wire everything up

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Game = require('./api/models/gameModel'),
+    Player = require('./api/models/playerModel'),
+    PlayerGame = require('./api/models/playerGameModel'),
+    bodyParser = require('body-parser');
+
+// mongoose config
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/resistats');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes
+require('./api/routes/gameRoutes')(app);
+require('./api/routes/playerRoutes')(app);
 
 app.listen(port);
 
